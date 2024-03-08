27b575efbe6b861b43b45ccfad616d3494331c32
hyellm <yhl9779@naver.com>
2024-03-08 16:59:23 +0900

    new TMAP API key

--- Server/TCP_Test/server/constant.py
+++ Server/TCP_Test/server/constant.py
@@ -1,5 +1,6 @@
 
-TMAP_APPKEY = "P56mSqdnljg7fNjmu2BD5I1DEutBPcVazegTouZ8"
+TMAP_APPKEY = "wuLynpCfphaaJZkwoId0w4AWaJb9mbht1IVtIX0K"  # __ 혜림
+# TMAP_APPKEY = "P56mSqdnljg7fNjmu2BD5I1DEutBPcVazegTouZ8"  __ 서희
 
 HOST = "172.20.10.4"
 PORT = 9999

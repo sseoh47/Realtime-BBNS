02db2259c9684c43b13338430344fc5a1c89687a
seohee <108114351+sseoh47@users.noreply.github.com>
2024-03-19 21:45:05 +0900

    change key2

--- Server/TCP_Test/client/constant.py
+++ Server/TCP_Test/client/constant.py
@@ -1,9 +1,9 @@
 
-TMAP_APPKEY = "P56mSqdnljg7fNjmu2BD5I1DEutBPcVazegTouZ8"
-#
+#TMAP_APPKEY = "P56mSqdnljg7fNjmu2BD5I1DEutBPcVazegTouZ8"
+TMAP_APPKEY = "wuLynpCfphaaJZkwoId0w4AWaJb9mbht1IVtIX0K"  # __ 혜림
 #티맵키 교체 완료
 
-HOST = "172.20.10.4"
+HOST = "172.20.10.6"
 PORT = 9999
 
 BUFFER_SIZE = 1024

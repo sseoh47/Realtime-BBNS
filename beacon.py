e0a4fbf7f2c7439ab47a09af55482b2cbd25590a
seohee <108114351+sseoh47@users.noreply.github.com>
2024-05-01 13:42:28 +0900

    remove

--- beacon.py
+++ beacon.py
@@ -1,19 +1,17 @@
 from beacontools import BeaconScanner, IBeaconFilter
 import time
 
-#iBeacon 예제를 사용
+
 class Beacon:
     def __init__(self):
         print("beacon탐색 시작")
 
-    # bluetooth address(MAC), rssi(신호세기), major, minnor 등 값을 출력
-    # BeaconScanner 클래스를 만들 때 callback으로 들어가게되는 함수
     def callback(self, uuid, rssi, packet, additional_info):
         print("<%s, %d> %s %s" % (uuid, rssi, packet, additional_info))
-        # uuid로 현재 위치 먼저 판별
+
         if uuid=='station':
             print("영남대 정류장")
-            # 버스 검색
+       
             
         
         elif uuid=='bus':

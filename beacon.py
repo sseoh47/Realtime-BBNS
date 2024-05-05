fbe588847e0e7c2633b92632a90235403ff2cd26
hyellm <yhl9779@naver.com>
2024-05-05 17:18:34 +0900

    Update beacon.py

--- beacon.py
+++ beacon.py
@@ -3,5 +3,6 @@ from beacontools import BeaconScanner, IBeaconFilter
 def callback(bt_addr, rssi, packet, additional_info):
     print("<%s, %d> %s %s" % (bt_addr, rssi, packet, additional_info))
 
-scanner = BeaconScanner(callback, bt_device_id=0 ,device_filter=IBeaconFilter)
+my_ibeacon_filter = IBeaconFilter(uuid="e2c56db5-dffb-48d2-b060-d0f5a71096e0")
+scanner = BeaconScanner(callback, bt_device_id=0 ,device_filter=my_ibeacon_filter)
 scanner.start()

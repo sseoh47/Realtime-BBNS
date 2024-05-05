0c92c26c8c14c6089e7bae07f8c7a2cf7cb917bf
hyellm <yhl9779@naver.com>
2024-05-05 17:41:38 +0900

    Update beacon.py

--- beacon.py
+++ beacon.py
@@ -1,5 +1,7 @@
 from bluepy.btle import Scanner, DefaultDelegate
 
+desired_uuid = "e2c56db5-dffb-48d2-b060-d0f5a71096e0"
+
 class ScanDelegate(DefaultDelegate):
     def __init__(self):
         DefaultDelegate.__init__(self)
@@ -10,11 +12,13 @@ class ScanDelegate(DefaultDelegate):
         elif isNewData:
             print("New data from", dev.addr)
 
-scanner = Scanner().withDelegate(ScanDelegate())
-devices = scanner.scan(10.0)
+        for (adtype, desc, value) in dev.getScanData():
+            if desc == 'Complete 128b Services' and value.startswith('fd6f'):
+                if value == desired_uuid:
+                    print("Desired UUID found:")
+                    print("UUID:", value)
+                    print("RSSI:", dev.rssi)
+                    return
 
-for dev in devices:
-    for (adtype, desc, value) in dev.getScanData():
-        if desc == 'Complete 128b Services' and value.startswith('fd6f'):
-            print("UUID:", value)
-            print("RSSI:", dev.rssi)
+scanner = Scanner().withDelegate(ScanDelegate())
+devices = scanner.scan(10.0) 

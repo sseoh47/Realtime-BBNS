7f31c7327fc17a92e8d21f11ff5154cb6f8e9c8d
seohee <108114351+sseoh47@users.noreply.github.com>
2024-05-05 17:31:13 +0900

    change beaconex

--- beacon.py
+++ beacon.py
@@ -1,28 +1,20 @@
-# -*- coding: utf-8 -*-
 from bluepy.btle import Scanner, DefaultDelegate
-import struct
 
 class ScanDelegate(DefaultDelegate):
     def __init__(self):
         DefaultDelegate.__init__(self)
 
-def parse_ibeacon_packet(packet):
-    if len(packet) == 31:
-        uuid = packet[9:25]
-        major = struct.unpack(">h", packet[25:27])[0]
-        minor = struct.unpack(">h", packet[27:29])[0]
-        tx_power = struct.unpack("b", packet[29:30])[0]
-        print("UUID: {}, Major: {}, Minor: {}, Tx Power: {}".format(uuid.hex(), major, minor, tx_power))
-
+    def handleDiscovery(self, dev, isNewDev, isNewData):
+        if isNewDev:
+            print("New device:", dev.addr)
+        elif isNewData:
+            print("New data from", dev.addr)
 
 scanner = Scanner().withDelegate(ScanDelegate())
-
-print("Scanning for iBeacon devices...")
-devices = scanner.scan(10.0)
+devices = scanner.scan(10.0)  # 10초 동안 스캔
 
 for dev in devices:
     for (adtype, desc, value) in dev.getScanData():
-        if adtype == 255:
-            data = bytes(bytearray.fromhex(value))
-            if data[0:4] == b"\x4c\x00\x02\x15":
-                parse_ibeacon_packet(data)
+        if desc == 'Complete 128b Services' and value.startswith('fd6f'):
+            print("UUID:", value)
+            print("RSSI:", dev.rssi)

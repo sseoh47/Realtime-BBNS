95a297ba36cb52b1385c2f8d55ed011305214c2f
seohee <108114351+sseoh47@users.noreply.github.com>
2024-05-05 17:57:28 +0900

    for desired uuid

--- beacon.py
+++ beacon.py
@@ -1,18 +1,20 @@
 from bluepy.btle import Scanner, DefaultDelegate
 
+
 # 원하는 비콘의 UUID
 desired_uuid = "e2c56db5-dffb-48d2-b060-d0f5a71096e0" # 여기에 원하는 UUID를 넣으세요
-
 
 class ScanDelegate(DefaultDelegate):
     def __init__(self):
         DefaultDelegate.__init__(self)
 
     def handleDiscovery(self, dev, isNewDev, isNewData):
-
         for (adtype, desc, value) in dev.getScanData():
-            print("UUID:", value)
-            print("RSSI:", dev.rssi)
+            if value == desired_uuid:
+                print("Desired UUID found:")
+                print("UUID:", value)
+                print("RSSI:", dev.rssi)
+                return
 
 scanner = Scanner().withDelegate(ScanDelegate())
-devices = scanner.scan(20.0)  # 10초 동안 스캔
+devices = scanner.scan(30.0) 

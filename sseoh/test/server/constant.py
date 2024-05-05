5388b025d26a7617a2315bd9aed74eeeb724bcac
hyellm <yhl9779@naver.com>
2024-05-05 18:51:32 +0900

    host change

--- sseoh/test/server/constant.py
+++ sseoh/test/server/constant.py
@@ -1,7 +1,7 @@
 TMAP_APPKEY = "wuLynpCfphaaJZkwoId0w4AWaJb9mbht1IVtIX0K"  # __ 혜림
 # TMAP_APPKEY = "P56mSqdnljg7fNjmu2BD5I1DEutBPcVazegTouZ8"  __ 서희
 
-HOST = "172.20.10.5"
+HOST = "172.20.10.4"
 PORT = 5555
 
 BUFFER_SIZE = 1024

b98a40911f639ef51d564e3fe472c81152fe7ad0
seohee <108114351+sseoh47@users.noreply.github.com>
2024-05-05 17:48:38 +0900

    change beacon

--- beacon.py
+++ beacon.py
@@ -1,22 +1,21 @@
 from bluepy.btle import Scanner, DefaultDelegate
 
-desired_uuid = "e2c56db5-dffb-48d2-b060-d0f5a71096e0"
+
+# 원하는 비콘의 UUID
+desired_uuid = "e2c56db5-dffb-48d2-b060-d0f5a71096e0" # 여기에 원하는 UUID를 넣으세요
 
 class ScanDelegate(DefaultDelegate):
     def __init__(self):
         DefaultDelegate.__init__(self)
 
     def handleDiscovery(self, dev, isNewDev, isNewData):
-        if isNewDev:
-            print("New device:", dev.addr, "RSSI: ", dev.rssi)
-        elif isNewData:
-            print("New data from", dev.addr, "RSSI: ", dev.rssi)
+        for (adtype, desc, value) in dev.getScanData():
+            if desc == 'Complete 128b Services' and value.startswith('fd6f'):
+                if value == desired_uuid:
+                    print("Desired UUID found:")
+                    print("UUID:", value)
+                    print("RSSI:", dev.rssi)
+                    return
 
 scanner = Scanner().withDelegate(ScanDelegate())
 devices = scanner.scan(10.0)  # 10초 동안 스캔
-
-for dev in devices:
-    for (adtype, desc, value) in dev.getScanData():
-        if desc == 'Complete 128b Services' and value.startswith('fd6f'):
-            print("UUID:", value)
-            print("RSSI:", dev.rssi)

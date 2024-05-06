83b66dc5082790b84ad7e34372e1060617aee9f5
seohee <108114351+sseoh47@users.noreply.github.com>
2024-05-07 01:14:55 +0900

    change send_beacon

--- sseoh/test/client/client.py
+++ sseoh/test/client/client.py
@@ -7,21 +7,23 @@ from beacon import*
 import os
 
 class Client:
-    def __init__(self):
+    def __init__(self,server_host, server_port,):
         print("클라이언트 연결됨")
+         # 서버에 접속
+        self.sock = socket.create_connection((server_host, server_port))
+        print("서버에 연결되었습니다.")
+
+        # 메시지 수신을 위한 스레드 시작
+        receive_thread = threading.Thread(target=self.receive_messages)
+        receive_thread.start()
+    # init과 send_beacon 다시보기. 기존 send_beacon 안에 create_connection이 있었는데
+    # send_beacon은 while문 내부에 있었음.
 
     # 비콘 이름, rssi는 비콘함수 추가 후 바꾸기
     def send_beacon(self, server_host, server_port, beacon_name, rssi):
         print("send_beacon함수")
         try:
-            # 서버에 접속
-            self.sock = socket.create_connection((server_host, server_port))
-            print("서버에 연결되었습니다.")
-
-            # 메시지 수신을 위한 스레드 시작
-            receive_thread = threading.Thread(target=self.receive_messages)
-            receive_thread.start()
-
+           
             while True:
                 # 비콘 이름과 RSSI 값을 문자열로 결합하여 서버에 전송
                 print("비콘정보전송")
@@ -33,11 +35,11 @@ class Client:
             print('프로그램이 사용자에 의해 중단되었습니다.')
         except Exception as e:
             print('Error:', e)
-        finally:
-            # 소켓 닫기
-            if self.sock:
-                self.sock.close()
-            print("클라이언트 종료")
+        # finally:
+        #     # 소켓 닫기
+        #     if self.sock:
+        #         self.sock.close()
+        #     print("클라이언트 종료")
 
     def receive_messages(self):
         try:

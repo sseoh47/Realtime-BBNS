a03dd9ecc625f66dca54560889b6c15851569daa
seohee <108114351+sseoh47@users.noreply.github.com>
2024-05-08 21:01:35 +0900

    Update client.py

--- sseoh/test/client/client.py
+++ sseoh/test/client/client.py
@@ -82,10 +82,10 @@ class Client:
                         text_to_speech(current_beacon_name)
                         # pip install playsound==1.2.2
                   
-                        audio = AudioSegment.from_file("audio", format='mp3')
+                        audio = AudioSegment.from_file(AUDIO, format='mp3')
                         play(audio)
 
-                        # playsound.playsound(AUDIO)
+                        playsound.playsound(AUDIO)
                         print("mp3 재생")
                         #os.remove(AUDIO) #생성된 파일 제거 # 다중 접속할때 permisson denined.해결용
                         self.previous_beacon_name = current_beacon_name

d7dac1612d5f9dc3fe315baec1899de8b4ed184c
seohee <108114351+sseoh47@users.noreply.github.com>
2024-05-08 22:58:05 +0900

    wav update

--- sseoh/test/client/client.py
+++ sseoh/test/client/client.py
@@ -84,7 +84,7 @@ class Client:
                         # play(audio)
                         # sound()
                         playsound.playsound(AUDIO)
-                        print("mp3 재생")
+                        print("wav 재생")
                         #os.remove(AUDIO) #생성된 파일 제거 # 다중 접속할때 permisson denined.해결용
                         self.previous_beacon_name = current_beacon_name
 

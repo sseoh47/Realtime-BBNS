4d3da9f683d6fe07d4896669f7d8bf034c4f6c66
seohee <108114351+sseoh47@users.noreply.github.com>
2024-05-09 15:18:02 +0900

    update host

--- Server/TCP_Test/client/constant.py
+++ Server/TCP_Test/client/constant.py
@@ -3,7 +3,7 @@
 TMAP_APPKEY = "wuLynpCfphaaJZkwoId0w4AWaJb9mbht1IVtIX0K"  # __ 혜림
 #티맵키 교체 완료
 
-HOST = "172.20.10.6"
+HOST = "172.20.10.4"
 PORT = 9999
 
 BUFFER_SIZE = 1024
--- Server/TCP_Test/server/constant.py
+++ Server/TCP_Test/server/constant.py
@@ -2,7 +2,7 @@
 TMAP_APPKEY = "wuLynpCfphaaJZkwoId0w4AWaJb9mbht1IVtIX0K"  # __ 혜림
 # TMAP_APPKEY = "P56mSqdnljg7fNjmu2BD5I1DEutBPcVazegTouZ8"  __ 서희
 #HOST = ""
-HOST = "172.20.10.6"
+HOST = "172.20.10.4"
 PORT = 9999
 
 BUFFER_SIZE = 1024

b06e07dcb9951d06b5b6d23406cea6c33a5100b5
seohee <108114351+sseoh47@users.noreply.github.com>
2024-05-09 17:34:12 +0900

    Update client.py

--- sseoh/test/client/client.py
+++ sseoh/test/client/client.py
@@ -56,7 +56,7 @@ class Client:
                         print("current_beacon_name:",current_beacon_name)
                         text_to_speech(current_beacon_name)
                         # pip install playsound==1.2.2
-
+                        playsound(AUDIO)
                         #os.remove(AUDIO) #생성된 파일 제거 # 다중 접속할때 permisson denined.해결용
                         self.previous_beacon_name = current_beacon_name
 

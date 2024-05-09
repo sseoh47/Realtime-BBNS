c6c47d48c9584492afcaa5330200a93adce26391
seohee <108114351+sseoh47@users.noreply.github.com>
2024-05-09 15:42:59 +0900

    Update playsound.py

--- sseoh/test/client/playsound.py
+++ sseoh/test/client/playsound.py
@@ -1,4 +1,10 @@
-import playsound
+import pygame
 
+def play_audio_non_blocking(file_path):
+    pygame.mixer.init()  # Pygame의 mixer 모듈을 초기화합니다.
+    pygame.mixer.music.load(file_path)  # 재생할 오디오 파일을 로드합니다.
+    pygame.mixer.music.play()  # 오디오 파일을 재생합니다.
 
-playsound('audio//output.wav', block=False)
+if __name__ == "__main__":
+    audio_file_path = 'audio//output.wav'
+    play_audio_non_blocking(audio_file_path)

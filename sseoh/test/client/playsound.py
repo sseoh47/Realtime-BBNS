ce79377f7a8f0d36cc2525414308aa99d12f41ce
seohee <108114351+sseoh47@users.noreply.github.com>
2024-05-09 15:54:49 +0900

    .

--- sseoh/test/client/playsound.py
+++ sseoh/test/client/playsound.py
@@ -1,15 +1,4 @@
-import pygame
-import time
+from playsound import playsound
+from constant import AUDIO
 
-
-def play_audio_non_blocking(file_path):
-    pygame.init()  # pygame 전체를 초기화
-    pygame.mixer.init()
-    pygame.mixer.music.load(file_path)
-    time.sleep(1.5)
-    pygame.mixer.music.play()
-
-if __name__ == "__main__":
-    audio_file_path = 'audio/output.wav'
-    play_audio_non_blocking(audio_file_path)
-
+playsound(AUDIO)

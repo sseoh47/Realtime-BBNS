9c3152258e6870cf239715c8fdc44e39764e44cc
hyellm <yhl9779@naver.com>
2024-05-14 20:07:40 +0900

    Update beacon.py

--- sseoh/test/client/beacon.py
+++ sseoh/test/client/beacon.py
@@ -6,28 +6,42 @@ import os
 
 
 BUS = "BUS"  
-STATION = "YU_UNIV" #다시 확인하기
+STATION = "YU_UNIV"  # 다시 확인하기
 
 class ScanDelegate(DefaultDelegate):
     def __init__(self, client):
         DefaultDelegate.__init__(self)
         self.client = client
+        self.search_mode = 'STATION'  # ++5/14++ 처음에는 STATION 비콘을 찾도록 설정
         print("beacon init")
 
     def handleDiscovery(self, dev, isNewDev, isNewData):
         try:
             for (adtype, desc, value) in dev.getScanData():
-                if adtype == 9 and value in [BUS, STATION]:
-                    #print("Name:", value)
-                    #print("RSSI:", dev.rssi)
-                    # 직접 send_beacon 호출
-                    self.client.send_beacon(value, dev.rssi)
+                # ++5/14++
+                if adtype == 9:
+                    if self.search_mode == 'STATION' and value == STATION:
+                        # STATION 비콘 발견
+                        print("STATION Beacon found")
+                        self.client.send_beacon(value, dev.rssi)
+                        self.search_mode = 'BUS'  # 이제 BUS 비콘을 찾도록 상태 변경
+                    elif self.search_mode == 'BUS' and value == BUS:
+                        # BUS 비콘 발견
+                        print("BUS Beacon found")
+                        self.client.send_beacon(value, dev.rssi)
+                # ++++
+                
+                # if adtype == 9 and value in [BUS, STATION]:
+                #     #print("Name:", value)
+                #     #print("RSSI:", dev.rssi)
+                #     # 직접 send_beacon 호출
+                #     self.client.send_beacon(value, dev.rssi)
 
         except KeyboardInterrupt:
             print("Scanning stopped")
+            
         except Exception as e:
             print(f"Error occurred while receiving message: {e}")
-
 
     def send_beacon_in_thread(self, beacon_name, rssi):
         if self.current_thread and self.current_thread.is_alive():
